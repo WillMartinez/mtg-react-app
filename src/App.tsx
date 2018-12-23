596f684674f23d16baefae9df0c7d8e948cbb77e
Will Martinez <williamjmartinez@me.com>
2018-12-23 14:46:27 -0600

    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
 import * as React from 'react';
 import './App.css';
-import { AppBar, IconButton, Typography, Button, Toolbar } from '@material-ui/core';
+import {
+  AppBar,
+  IconButton,
+  Typography,
+  Button,
+  Toolbar,
+  Grid,
+  withStyles,
+  Theme,
+  createStyles,
+  MuiThemeProvider
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
+import theme from './theme';
 
+// Theme-dependent styles
+const styles = ({ palette, spacing }: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    grow: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20
+    }
+  });
 
 class App extends React.Component {
   public render() {
-    return <AppBar position="static">
-    <Toolbar>
-      <IconButton color="inherit" aria-label="Menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" color="inherit">
-        News
-      </Typography>
-      <Button color="inherit">Login</Button>
-    </Toolbar>
-    </AppBar>
+    return (
+      <Grid container={true}>
+        <MuiThemeProvider theme={theme}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit">
+                Draft
+              </Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+        </MuiThemeProvider>
+      </Grid>
+    );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
---
+++ src/theme.jsx
@@ -0,0 +1,36 @@
+import { createMuiTheme } from '@material-ui/core';
+
+export default createMuiTheme({
+  palette: {
+    common: { black: '#000', white: '#fff' },
+    background: { paper: '#fff', default: '#fafafa' },
+    primary: {
+      light: '#7986cb',
+      main: 'rgba(63, 181, 103, 1)',
+      dark: '#303f9f',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#ff4081',
+      main: 'rgba(64, 110, 160, 1)',
+      dark: '#c51162',
+      contrastText: '#fff'
+    },
+    error: {
+      light: '#e57373',
+      main: '#f44336',
+      dark: '#d32f2f',
+      contrastText: '#fff'
+    },
+    text: {
+      primary: 'rgba(0, 0, 0, 0.87)',
+      secondary: 'rgba(0, 0, 0, 0.54)',
+      disabled: 'rgba(0, 0, 0, 0.38)',
+      hint: 'rgba(0, 0, 0, 0.38)'
+    }
+  },
+
+  typography: {
+    useNextVariants: true
+  }
+});
